0934cee4a084c85a069f3d34519132818f73ff13
Emzzy241 <emzzyoluwole@gmail.com>
2024-08-02 05:20:10 +0100

    add controllers

---
+++ Sign-up/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sign_up.Models;
+
+namespace Sign_up.Controllers;
+
+public class HomeController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+}
